b6eb9120f999bc7742e918a491b6297e9edca124
Henrique Kioshi Yamauchi <101335626+hkioshi@users.noreply.github.com>
2023-03-17 10:44:54 -0300

    botao 1 e 2

---
+++ Cartagena - Soacha/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Cartagena___Soacha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListarPartidas = new System.Windows.Forms.Button();
+            this.lstPartidas = new System.Windows.Forms.ListBox();
+            this.btnSelecionarPartida = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnListarPartidas
+            // 
+            this.btnListarPartidas.Location = new System.Drawing.Point(7, 12);
+            this.btnListarPartidas.Name = "btnListarPartidas";
+            this.btnListarPartidas.Size = new System.Drawing.Size(194, 140);
+            this.btnListarPartidas.TabIndex = 0;
+            this.btnListarPartidas.Text = "Listar Partidas";
+            this.btnListarPartidas.UseVisualStyleBackColor = true;
+            this.btnListarPartidas.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstPartidas
+            // 
+            this.lstPartidas.FormattingEnabled = true;
+            this.lstPartidas.ItemHeight = 16;
+            this.lstPartidas.Location = new System.Drawing.Point(7, 158);
+            this.lstPartidas.Name = "lstPartidas";
+            this.lstPartidas.Size = new System.Drawing.Size(194, 212);
+            this.lstPartidas.TabIndex = 2;
+            this.lstPartidas.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnSelecionarPartida
+            // 
+            this.btnSelecionarPartida.Location = new System.Drawing.Point(7, 376);
+            this.btnSelecionarPartida.Name = "btnSelecionarPartida";
+            this.btnSelecionarPartida.Size = new System.Drawing.Size(194, 59);
+            this.btnSelecionarPartida.TabIndex = 3;
+            this.btnSelecionarPartida.Text = "SelecionarPartida";
+            this.btnSelecionarPartida.UseVisualStyleBackColor = true;
+            this.btnSelecionarPartida.Click += new System.EventHandler(this.btnSelecionarPartida_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(207, 181);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 16);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnSelecionarPartida);
+            this.Controls.Add(this.lstPartidas);
+            this.Controls.Add(this.btnListarPartidas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListarPartidas;
+        private System.Windows.Forms.ListBox lstPartidas;
+        private System.Windows.Forms.Button btnSelecionarPartida;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Cartagena - Soacha/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CartagenaServer;
+
+namespace Cartagena___Soacha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string retorno = Jogo.ListarPartidas("T");
+            
+
+
+            retorno = retorno.Replace("\r", "");
+
+
+            string[] partidas = retorno.Split('\n');
+            for(int i = 0;i < partidas.Length ;i++)
+            {
+                lstPartidas.Items.Add(partidas[i]);
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSelecionarPartida_Click(object sender, EventArgs e)
+        {
+            string partidas = lstPartidas.SelectedItem.ToString();
+            string[] itens = partidas.Split(new char[] {',' });
+
+            int idPartida = Convert.ToInt32(itens[0]);
+            string nomePartida = itens[1];
+            string dataPartida = itens[2];
+            string status = itens[3];
+
+            lblStatus.Text = $" Id: {idPartida}\n Nome: {nomePartida} \n status: {status}";
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
